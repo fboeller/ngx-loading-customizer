090843cd739076d552eb69b01330df20d33aabed
fboeller <fabianboeller@googlemail.com>
2021-02-28 22:58:05 +0100

    Complete public-api

--- projects/ngx-loadable/src/public-api.ts
+++ projects/ngx-loadable/src/public-api.ts
@@ -1,1 +1,9 @@
 export * from './lib/loadable.type';
+export * from './lib/loadable.functions';
+export * from './lib/loadable.constructors';
+export * from './lib/from.functions';
+
+export * from './lib/loadable/loadable.module';
+export * from './lib/loadable/is-loading.pipe';
+export * from './lib/loadable/loading-state.pipe';
+export * from './lib/loadable/loadable-error.type';
