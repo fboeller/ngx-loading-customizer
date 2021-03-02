73f67b6a9f9f77b0f815d392f42340f98b6db546
fboeller <fabianboeller@googlemail.com>
2021-03-02 19:33:39 +0100

    Use loading spinner component

--- projects/example-app/src/app/app.module.ts
+++ projects/example-app/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { LoadableModule } from 'projects/ngx-loadable/src/public-api';
 import { SwitchCaseExampleComponent } from './components/switch-case-example/switch-case-example.component';
 import { LoadableComponentExampleComponent } from './components/loadable-component-example/loadable-component-example.component';
+import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SwitchCaseExampleComponent,
     LoadableComponentExampleComponent,
+    LoadingSpinnerComponent,
   ],
   imports: [BrowserModule, LoadableModule],
   providers: [],
