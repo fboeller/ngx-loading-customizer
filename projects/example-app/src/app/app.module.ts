9a47dda29a636b1d4705c0f481702547aa26ee4e
fboeller <fabianboeller@googlemail.com>
2021-03-07 21:16:52 +0100

    Use hash based routing for GitHub pages

--- projects/example-app/src/app/app.module.ts
+++ projects/example-app/src/app/app.module.ts
@@ -9,58 +9,61 @@ import { SidebarComponent } from './sidebar/sidebar.component';
   declarations: [AppComponent, SidebarComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([
-      {
-        path: 'examples',
-        children: [
-          {
-            path: 'custom-default-loading',
-            loadChildren: () =>
-              import(
-                './examples/custom-default-loading-example/custom-default-loading-example.module'
-              ).then((m) => m.CustomDefaultLoadingExampleModule),
-          },
-          {
-            path: 'ngrx',
-            loadChildren: () =>
-              import('./examples/ngrx-example/ngrx-example.module').then(
-                (m) => m.NgrxExampleModule
-              ),
-          },
-          {
-            path: 'custom-loading-template',
-            loadChildren: () =>
-              import(
-                './examples/custom-loading-template-example/custom-loading-template-example.module'
-              ).then((m) => m.CustomLoadingTemplateExampleModule),
-          },
-          {
-            path: 'default',
-            loadChildren: () =>
-              import('./examples/default-example/default-example.module').then(
-                (m) => m.DefaultExampleModule
-              ),
-          },
-          {
-            path: 'lightweight',
-            loadChildren: () =>
-              import(
-                './examples/lightweight-example/lightweight-example.module'
-              ).then((m) => m.LightweightExampleModule),
-          },
-          {
-            path: '',
-            pathMatch: 'full',
-            redirectTo: 'default',
-          },
-        ],
-      },
-      {
-        path: '',
-        pathMatch: 'full',
-        redirectTo: 'examples',
-      },
-    ]),
+    RouterModule.forRoot(
+      [
+        {
+          path: 'examples',
+          children: [
+            {
+              path: 'custom-default-loading',
+              loadChildren: () =>
+                import(
+                  './examples/custom-default-loading-example/custom-default-loading-example.module'
+                ).then((m) => m.CustomDefaultLoadingExampleModule),
+            },
+            {
+              path: 'ngrx',
+              loadChildren: () =>
+                import('./examples/ngrx-example/ngrx-example.module').then(
+                  (m) => m.NgrxExampleModule
+                ),
+            },
+            {
+              path: 'custom-loading-template',
+              loadChildren: () =>
+                import(
+                  './examples/custom-loading-template-example/custom-loading-template-example.module'
+                ).then((m) => m.CustomLoadingTemplateExampleModule),
+            },
+            {
+              path: 'default',
+              loadChildren: () =>
+                import(
+                  './examples/default-example/default-example.module'
+                ).then((m) => m.DefaultExampleModule),
+            },
+            {
+              path: 'lightweight',
+              loadChildren: () =>
+                import(
+                  './examples/lightweight-example/lightweight-example.module'
+                ).then((m) => m.LightweightExampleModule),
+            },
+            {
+              path: '',
+              pathMatch: 'full',
+              redirectTo: 'default',
+            },
+          ],
+        },
+        {
+          path: '',
+          pathMatch: 'full',
+          redirectTo: 'examples',
+        },
+      ],
+      { useHash: true }
+    ),
   ],
   bootstrap: [AppComponent],
 })
