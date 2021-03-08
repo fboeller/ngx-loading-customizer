ec902c481246146f3e364d1c9a800f90ab8c0b76
fboeller <fabianboeller@googlemail.com>
2021-03-08 20:25:49 +0100

    Complete public api

--- projects/ngx-loading-customizer/src/public-api.ts
+++ projects/ngx-loading-customizer/src/public-api.ts
@@ -2,16 +2,20 @@ export * from './lib/loadable.type';
 export * from './lib/loadable.functions';
 export * from './lib/loadable.constructors';
 
+export * from './lib/functions/flat-map.function';
+export * from './lib/functions/fold.function';
 export * from './lib/functions/from-promise.function';
 export * from './lib/functions/map.function';
+export * from './lib/functions/of.function';
 export * from './lib/functions/to-loadable.function';
 
-export * from './lib/angular/loading-customizer.module';
 export * from './lib/angular/loadable/loadable.component';
 export * from './lib/angular/loadable-error/loadable-error.component';
 export * from './lib/angular/loadable-loading/loadable-loading.component';
 export * from './lib/angular/is-loading.pipe';
-export * from './lib/angular/loading-state.pipe';
-export * from './lib/angular/loaded-value.pipe';
 export * from './lib/angular/loadable-error.type';
 export * from './lib/angular/loadable.tokens';
+export * from './lib/angular/loaded-value.pipe';
+export * from './lib/angular/loading-customizer.module';
+export * from './lib/angular/loading-state.pipe';
+export * from './lib/angular/module.options';
